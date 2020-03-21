c63211a5061f2dc49484df075d0f09992358b285
Stephanie Nemeth <stnemeth@microsoft.com>
2020-03-21 20:45:03 +0100

    more schema stuff

---
+++ models/Store.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim :true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+
+storeSchema.pre('save', function(next) {
+  // Don't use arrow function here since we need this 
+  // to refer to scope within the function aka store (function scope vs lexical scope)
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  next();
+})
+
+module.exports = mongoose.model('Store', storeSchema);
