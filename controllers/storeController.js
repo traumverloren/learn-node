cf023f7643b5ba60a1dfd7ee4e339b7c5acba303
Stephanie Nemeth <stnemeth@microsoft.com>
2020-03-23 11:35:29 +0100

    add stores route and view

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,16 +1,25 @@
-const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-  res.render('index');
-}
+  res.render("index");
+};
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: 'Add Store'});
-}
+  res.render("editStore", { title: "Add Store" });
+};
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body)
-  await store.save();
-  res.redirect('/');
-}
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successfully Created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // query db for list of all stores
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const storeController = require('../controllers/storeController')
-const { catchErrors } = require('../handlers/errorHandlers');
+const storeController = require("../controllers/storeController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
-router.get('/', storeController.homePage);
-router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
+router.get("/", catchErrors(storeController.getStores));
+router.get("/stores", catchErrors(storeController.getStores));
+router.get("/add", storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore));
 module.exports = router;
